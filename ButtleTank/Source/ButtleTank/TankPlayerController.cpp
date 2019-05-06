a94ece0f01812e7c1d4cd5ed2a7e1a25093c58df
DESKTOP-A65Q0S5\Vladimir <gamedevshttl@gmail.com>
2019-05-06 22:22:37 +0300

    Delegating to Components

---
+++ ButtleTank/Source/ButtleTank/TankPlayerController.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+#include "Tank.h"
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
---
+++ ButtleTank/Source/ButtleTank/TankPlayerController.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUTTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+
+	class ATank* GetControlledTank() const;
+	
+};
