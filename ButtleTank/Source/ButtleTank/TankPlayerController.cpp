a30dadea2f0a079f899d51365c43eeddf12aded4
DESKTOP-A65Q0S5\Vladimir <gamedevshttl@gmail.com>
2019-05-08 23:27:06 +0300

    Get HitLocation

--- ButtleTank/Source/ButtleTank/TankPlayerController.cpp
+++ ButtleTank/Source/ButtleTank/TankPlayerController.cpp
@@ -2,6 +2,7 @@
 
 #include "TankPlayerController.h"
 #include "Tank.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
 
 void ATankPlayerController::BeginPlay()
 {
@@ -15,4 +16,59 @@ ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
+
+void ATankPlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	AimTowardCrosshair();
+}
+
+void ATankPlayerController::AimTowardCrosshair()
+{
+	if (!GetControlledTank())
+		return;
+
+	FVector HitLocation;
+	if (GetSightRayHitLocation(HitLocation)) {
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+	}
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection)) {		
+		if (GetLookVectorHitLocation(LookDirection, HitLocation)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool ATankPlayerController::GetLookVectorHitLocation(const FVector& LookDirection, FVector& HitLocation) const
+{
+	auto StartLocation = PlayerCameraManager->GetCameraLocation();
+	auto EndLocation = StartLocation + LookDirection * LineTraceRange;
+
+	FHitResult HitResult;
+	if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECollisionChannel::ECC_Visibility)) {
+		HitLocation = HitResult.Location;
+		return true;
+	}
+	
+	HitLocation = FVector(0);
+	return false;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D & ScreenLocation, FVector & LookDirection) const
+{	
+	FVector CameraWorldLocation;
+	return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, CameraWorldLocation, LookDirection);
+}
+
 
--- ButtleTank/Source/ButtleTank/TankPlayerController.h
+++ ButtleTank/Source/ButtleTank/TankPlayerController.h
@@ -14,10 +14,25 @@ class BUTTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
-public:
-
+protected:
 	virtual void BeginPlay() override;
 
 	class ATank* GetControlledTank() const;
-	
+
+public:
+	virtual void Tick(float DeltaSeconds) override;
+
+	void AimTowardCrosshair();
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+	bool GetLookDirection(FVector2D& ScreenLocation, FVector& LookDirection) const;
+	bool GetLookVectorHitLocation(const FVector& LookDirection, FVector& HitLocation) const;
+
+	UPROPERTY(VisibleAnywhere)
+	float CrossHairXLocation = 0.5f;
+
+	UPROPERTY(VisibleAnywhere)
+	float CrossHairYLocation = 0.33333f;
+
+	UPROPERTY(VisibleAnywhere)
+	int LineTraceRange = 1000000;
 };
